f4c68e888bca05fcb8540936da3d712a6d87876c
OlgaKaratkevich <volhakaratkevich@gmail.com>
2024-10-11 01:14:52 +0300

    --

--- src/test/java/FileUploadTest.java
+++ src/test/java/FileUploadTest.java
@@ -1,2 +1,21 @@
-public class FileUploadTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+import static org.testng.Assert.assertEquals;
+
+public class FileUploadTest extends BaseTest {
+
+    @Test
+    public void fileUpload() {
+        driver.get("https://the-internet.herokuapp.com/upload");
+        File file = new File("src/test/resources/image.png");
+        String fileName = file.getName();
+        driver.findElement(By.id("file-upload")).sendKeys(file.getAbsolutePath());
+        WebElement upload = driver.findElement(By.id("file-submit"));
+        upload.click();
+        assertEquals(driver.findElement(By.id("uploaded-files")).getText(), fileName);
+    }
 }
